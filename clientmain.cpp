c41c680f9d272cf2c2af8e8d7cdbbd4af8d497e4
YURCFeng <1991023512@qq.com>
2023-04-22 21:04:39 +0800

    up

--- clientmain.cpp
+++ clientmain.cpp
@@ -23,8 +23,8 @@ int server_port = 5001;
 
 int main(int argc, char *argv[])
 {
-    int sock;
-	struct addrinfo hints,* p,* servinfo;
+	int sock;
+	struct addrinfo hints, *p, *servinfo;
 	char delim[] = ":";
 	char *Desthost = strtok(argv[1], delim); // dizhi
 	char *Destport = strtok(NULL, delim);
@@ -34,27 +34,37 @@ int main(int argc, char *argv[])
 	printf("Host %s, and port %d.\n", server_ip, server_port);
 
 	int rv;
-    memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC; 
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_DGRAM;
 
-	 if ((rv = getaddrinfo(Desthost, Destport, &hints, &servinfo)) != 0)
-    { 
-        perror("getaddrinfo fail");
-        exit(1);
-    }
-	for (p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+	if ((rv = getaddrinfo(Desthost, Destport, &hints, &servinfo)) != 0)
+	{
+		perror("getaddrinfo fail");
+		exit(1);
+	}
+	for (p = servinfo; p != NULL; p = p->ai_next)
+	{
+		if ((sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+		{
 			perror("talker: socket");
 			continue;
 		}
 		break;
 	}
-	if (p == NULL) {
+	if (p == NULL)
+	{
 		fprintf(stderr, "talker: failed to create socket\n");
 		return 2;
 	}
+	char myAddress[20];
 
+	struct sockaddr_in local_sin;
+	socklen_t local_sinlen = sizeof(local_sin);
+	getsockname(sock, (struct sockaddr *)&local_sin, &local_sinlen);
+
+	inet_ntop(local_sin.sin_family, &local_sin.sin_addr, myAddress, sizeof(myAddress));
+	printf("Connected to %s:%s  local %s:%d\n", Desthost, Destport, myAddress, ntohs(local_sin.sin_port));
 	/* Do magic */
 	char buf[1000];
 	// int sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
@@ -93,13 +103,13 @@ int main(int argc, char *argv[])
 	{
 		// 发送消息
 		printf("Sending message\n");
-		if (sendto(sock, &msg, sizeof(msg), 0,  p->ai_addr, p->ai_addrlen) < 0)
+		if (sendto(sock, &msg, sizeof(msg), 0, p->ai_addr, p->ai_addrlen) < 0)
 		{
 			perror("Failed to send message\n");
 			exit(1);
 		}
 		// memcpy
-		n = recvfrom(sock, &buf, sizeof(buf), 0,  p->ai_addr, &(p->ai_addrlen));
+		n = recvfrom(sock, &buf, sizeof(buf), 0, p->ai_addr, &(p->ai_addrlen));
 		if (n < 0)
 		{
 			printf("Timeout, retrying...\n");
@@ -129,13 +139,13 @@ int main(int argc, char *argv[])
 		else
 		{
 			memcpy(&res, buf, sizeof(buf));
-			printf("id: %u\n", ntohl(res.id));
-			printf("arith: %u\n", ntohl(res.arith));
-			printf("inValue1: %u\n", ntohl(res.inValue1));
-			printf("inValue2: %u\n", ntohl(res.inValue2));
-			printf("inResult: %u\n", ntohl(res.inResult));
-			printf("flValue1: %u\n", res.flValue1);
-			printf("flValue2: %u\n", res.flValue2);
+			// printf("id: %u\n", ntohl(res.id));
+			// printf("arith: %u\n", ntohl(res.arith));
+			// printf("inValue1: %u\n", ntohl(res.inValue1));
+			// printf("inValue2: %u\n", ntohl(res.inValue2));
+			// printf("inResult: %u\n", ntohl(res.inResult));
+			// printf("flValue1: %u\n", res.flValue1);
+			// printf("flValue2: %u\n", res.flValue2);
 			// printf("flResult: %u\n", res.flResult);
 
 			int arith = ntohl(res.arith);
@@ -143,8 +153,9 @@ int main(int argc, char *argv[])
 			double f1, f2, fresult;
 			int i1, i2;
 			uint32_t iresult;
-			char op[10];
-			char bufline[100];
+			char op[100];
+			memset(&op, 0, sizeof op);
+
 			if (arith >= 5 & arith <= 8)
 			{
 				judge = 1;
@@ -164,21 +175,26 @@ int main(int argc, char *argv[])
 				if (arith == 5)
 				{
 					fresult = f1 + f2;
+					strncpy(op, "fadd", sizeof(op));
 				}
 				else if (arith == 6)
 				{
 					fresult = f1 - f2;
+					strncpy(op, "fsub", sizeof(op));
 				}
 				else if (arith == 7)
 				{
 					fresult = f1 * f2;
+					strncpy(op, "fmul", sizeof(op));
 				}
 				else if (arith == 8)
 				{
 					fresult = f1 / f2;
+					strncpy(op, "fdiv", sizeof(op));
 				}
 				// printf("%s %8.8g %8.8g = %8.8g\n", op, f1, f2, fresult);
-				// printf("Calculated the result to %d\n", fresult);
+				printf("ASSIGNMENT: %s %8.8g %8.8g\n", f1,op,f2);
+				printf("Calculated the result to %d\n", fresult);
 				res.flResult = fresult;
 				res.type = htons(2);
 				int numbytes;
@@ -241,28 +257,32 @@ int main(int argc, char *argv[])
 				if (arith == 1)
 				{
 					iresult = i1 + i2;
+					strncpy(op, "add", sizeof(op));
 				}
 				else if (arith == 2)
 				{
 					iresult = i1 - i2;
+					strncpy(op, "sub", sizeof(op));
 				}
 				else if (arith == 3)
 				{
 					iresult = i1 * i2;
+					strncpy(op, "mul", sizeof(op));
 				}
 				else if (arith == 4)
 				{
 					iresult = i1 / i2;
+					strncpy(op, "div", sizeof(op));
 				}
 				else
 				{
 					printf("No match\n");
 				}
 				// printf("%s %d %d = %d \n",op,i1,i2,iresult);
-				// printf("Calculated the result to %d\n", iresult);
+				printf("ASSIGNMENT: %s %d %d\n", op,i1,i2);
+				printf("Calculated the result to %d\n", iresult);
 				res.inResult = htonl(iresult);
 				res.type = htons(2);
-				int numbytes;
 				// printf("type: %u\n", ntohs(res.type));
 				printf("inResult: %u\n", ntohl(res.inResult));
 
@@ -273,7 +293,7 @@ int main(int argc, char *argv[])
 				{
 					// 发送消息
 					printf("Sending message\n");
-					numbytes = sendto(sock, &res, sizeof(res), 0,p->ai_addr, p->ai_addrlen);
+					int numbytes = sendto(sock, &res, sizeof(res), 0, p->ai_addr, p->ai_addrlen);
 					if (numbytes < 0)
 					{
 						perror("Failed to send message");
@@ -281,7 +301,7 @@ int main(int argc, char *argv[])
 					}
 
 					// printf("sent %d bytes\n", numbytes);
-					int n = recvfrom(sock, &msg_after, sizeof(msg_after), 0, p->ai_addr,&(p->ai_addrlen));
+					int n = recvfrom(sock, &msg_after, sizeof(msg_after), 0, p->ai_addr, &(p->ai_addrlen));
 					if (n < 0)
 					{
 						printf("Timeout, retrying...\n");
